4960ada15d4da8fae8145a1eb39d8821fa376a04
Derek Chia <t0915811@kelaberetiv.sg>
2018-08-21 10:24:40 +0800

    Config GPU

--- experiment.py
+++ experiment.py
@@ -149,6 +149,13 @@ def process_sentences(epoch, data, model, is_training, learningrate, config, nam
 
 
 def run_experiment(config_path):
+
+    # gpus = ['/device:GPU:{}'.format(n) for n in range(8)]
+
+    # for gpu in gpus:
+    #     with tf.device(gpu):
+    #         print(gpu)
+
     config = parse_config("config", config_path)
     temp_model_path = config_path + ".model"
     if "random_seed" in config:
@@ -169,13 +176,19 @@ def run_experiment(config_path):
         for path_test in config["path_test"].strip().split(":"):
             data_test += read_input_files(path_test)
     
-    model = MLTModel(config)
+    # gpus = ['/device:GPU:{}'.format(n) for n in range(8)]
 
-    model.build_vocabs(data_train, data_dev, data_test, config["preload_vectors"])
-    model.construct_network()
-    model.initialize_session()
-    if config["preload_vectors"] != None:
-        model.preload_word_embeddings(config["preload_vectors"])
+    # for gpu in gpus:
+    #     with tf.device(gpu):
+    #         print(gpu)
+
+    with tf.device('/device:GPU:2'):
+        model = MLTModel(config)
+        model.build_vocabs(data_train, data_dev, data_test, config["preload_vectors"])
+        model.construct_network()
+        model.initialize_session()
+        if config["preload_vectors"] != None:
+            model.preload_word_embeddings(config["preload_vectors"])
 
     print("parameter_count: " + str(model.get_parameter_count()))
     print("parameter_count_without_word_embeddings: " + str(model.get_parameter_count_without_word_embeddings()))
@@ -191,34 +204,29 @@ def run_experiment(config_path):
             print("current_learningrate: " + str(learningrate))
             random.shuffle(data_train)
 
-            gpus = ['/device:GPU:{}'.format(n) for n in range(8)]
+            results_train = process_sentences(epoch, data_train, model, is_training=True, learningrate=learningrate, config=config, name="train")
 
-            for gpu in gpus:
-                with tf.device(gpu):
-                    print(gpu)
-                    results_train = process_sentences(epoch, data_train, model, is_training=True, learningrate=learningrate, config=config, name="train")
+            if data_dev != None:
+                results_dev = process_sentences(epoch, data_dev, model, is_training=False, learningrate=0.0, config=config, name="dev")
 
-                    if data_dev != None:
-                        results_dev = process_sentences(epoch, data_dev, model, is_training=False, learningrate=0.0, config=config, name="dev")
+                if math.isnan(results_dev["dev_cost_sum"]) or math.isinf(results_dev["dev_cost_sum"]):
+                    raise ValueError("Cost is NaN or Inf. Exiting.")
 
-                        if math.isnan(results_dev["dev_cost_sum"]) or math.isinf(results_dev["dev_cost_sum"]):
-                            raise ValueError("Cost is NaN or Inf. Exiting.")
+                if (epoch == 0 or (model_selector_type == "high" and results_dev[model_selector] > best_selector_value) 
+                               or (model_selector_type == "low" and results_dev[model_selector] < best_selector_value)):
+                    best_epoch = epoch
+                    best_selector_value = results_dev[model_selector]
+                    model.saver.save(model.session, temp_model_path, latest_filename=os.path.basename(temp_model_path)+".checkpoint")
+                print("best_epoch: " + str(best_epoch))
 
-                        if (epoch == 0 or (model_selector_type == "high" and results_dev[model_selector] > best_selector_value) 
-                                       or (model_selector_type == "low" and results_dev[model_selector] < best_selector_value)):
-                            best_epoch = epoch
-                            best_selector_value = results_dev[model_selector]
-                            model.saver.save(model.session, temp_model_path, latest_filename=os.path.basename(temp_model_path)+".checkpoint")
-                        print("best_epoch: " + str(best_epoch))
+                if config["stop_if_no_improvement_for_epochs"] > 0 and (epoch - best_epoch) >= config["stop_if_no_improvement_for_epochs"]:
+                    break
 
-                        if config["stop_if_no_improvement_for_epochs"] > 0 and (epoch - best_epoch) >= config["stop_if_no_improvement_for_epochs"]:
-                            break
+                if (epoch - best_epoch) > 3:
+                    learningrate *= config["learningrate_decay"]
 
-                        if (epoch - best_epoch) > 3:
-                            learningrate *= config["learningrate_decay"]
-
-                    while config["garbage_collection"] == True and gc.collect() > 0:
-                        pass
+            while config["garbage_collection"] == True and gc.collect() > 0:
+                pass
 
         if data_dev != None and best_epoch >= 0:
             # loading the best model so far
